cdfcc39bf50296dceba572ac270997cecc908155
Dwiky Bagus Prasetyo <18102119@ittelkom-pwt.ac.id>
2021-06-28 20:31:13 +0700

    update koneksi.php

--- RnC/koneksi.php
+++ RnC/koneksi.php
@@ -1,9 +1,23 @@
 <?php
+	/*koneksi lokal
 	 $servername = "localhost";
 	 $username = "root";
 	 $password = "";
 	 $dbname = "db_rnc";
+	 */
 
+	 /*koneksi subdomain 2
+	 $servername = "localhost";
+	 $username = "kelasti2_18102119";
+	 $password = "Dwikybp1423@";
+	 $dbname = "kelasti2_db_rnc"; 
+	 */
+
+	 //koneksi subdomain1
+	 $servername = "localhost";
+	 $username = "kelasti2_prak9";
+	 $password = "pusingsekali";
+	 $dbname = "kelasti2_db_rnc";
 
 	 // Create connection
 	 $conn = new mysqli($servername, $username, $password, $dbname);
